253e4feb08338249c25087033c9948538c364473
chikeket <minyounki@gmail.com>
2025-09-02 17:50:47 +0900

    server-250902

--- 04_mysql/app.js
+++ 04_mysql/app.js
@@ -4,11 +4,17 @@ const parser = require("body-parser");
 const sql = require("./sql");
 const prodSql = require("./sql/sql"); //  {productList:{}, productDetail:{}}
 const cors = require("cors");
+const fs = require("fs");
 // console.log(prodSql["productList"].query);
 
 const app = express();
 app.use(parser.urlencoded()); // x-www-form-urlencoded
-app.use(parser.json());
+
+app.use(
+  express.json({
+    limit: "10mb",
+  })
+);
 app.use(cors());
 app.get("/", (req, resp) => {
   resp.send("/ 실행");
@@ -20,12 +26,40 @@ app.post("/api/:alias", async (req, resp) => {
   let param = req.body.param;
   try {
     let result = await sql.execute(search, param);
-
+    console.log(result);
     resp.json(result);
   } catch {
     console.log(err);
     resp.json({ retCode: "Error" });
   }
+});
+
+app.post("/upload/:file_name", (req, resp) => {
+  let file_name = req.params.file_name;
+  let data = req.body.param;
+  // console.log(data);
+  // console.log(file_name);
+  fs.writeFile(__dirname + "/uploads/" + file_name, data, "base64", (err) => {
+    if (err) {
+      resp.send(err);
+      return;
+    }
+    resp.send("ok");
+  });
+});
+
+//이미지 링크정보
+app.get("/download/:product_id/:path", (req, resp) => {
+  let product_id = req.params.product_id;
+  let path = req.params.path; //keyboard.jpg
+  console.log(path);
+  resp.header("Content-Type", `image/${path.substring(path.lastIndexOf("."))}`);
+  const filepath = `${__dirname}/uploads/${product_id}/${path}`;
+  if (!fs.existsSync(filepath)) {
+    resp.send("없다 이자식아");
+    return;
+  }
+  fs.createReadStream(filepath).pipe(resp);
 });
 
 // 고객목록.
